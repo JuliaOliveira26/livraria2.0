1dbe15606301e50078f9346cb5e836cac8f64587
JuliaOliveira26 <julinhalves2006@gmail.com>
2024-03-18 14:31:32 -0300

    Formatação de código com isort e black

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 class Categoria(models.Model):
-    descriçao = models.CharField(max_length= 100)
+    descriçao = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'{self.descriçao} ({self.id})'
+        return f"{self.descriçao} ({self.id})"
